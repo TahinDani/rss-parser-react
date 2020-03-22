58a9fbdb73153d7eac16f6ff211998b6ee13d1e9
Daniel Tahin <tahin.daniel@gmail.com>
2020-03-22 11:02:50 +0100

    News background color and "no news" warning

--- src/components/News.js
+++ src/components/News.js
@@ -3,17 +3,40 @@ import '../style/News.css'
 
 class News extends Component {
 	
+	/* 
+	index #f69220 
+	hvg #e25900
+	444 #feff73
+	24.hu #63667a
+	*/
+
+	getBackgroundColor = (site) => {
+		switch (site) {
+			case "Index - 24óra":
+				return "#f69220"
+			case "444":
+				return "#feff73"
+			case "hvg.hu RSS":
+				return "#e25900"
+			case "24.hu":
+				return "#63667a"
+			default:
+				return "#a0a0a0"
+		}
+	}
+	
 	render() {
-		let imageUrl = this.props.newsItem.imageUrl
+		const imageUrl = this.props.newsItem.imageUrl
+		const siteBackgroundColor = this.getBackgroundColor(this.props.newsItem.site)
 		return (
-			<div className='News d-flex'>
+			<div className='News d-flex' style={{backgroundColor:siteBackgroundColor}}>
 				
 				<div className="News-image" style={{backgroundImage: `url(${imageUrl})`}}>
 					<div className="News-category">{this.props.newsItem.category}</div>
 				</div> 
 				
 				<div className="News-text">
-					<p><strong>{this.props.newsItem.site}</strong> {this.props.newsItem.title}</p>
+					<p className="News-title">{this.props.newsItem.title}</p>
 					<a href={this.props.newsItem.link}>{this.props.newsItem.content || 'Link'}</a>
 				</div>
 			</div>
--- src/components/NewsContainer.js
+++ src/components/NewsContainer.js
@@ -6,16 +6,20 @@ import '../style/NewsContainer.css'
 class NewsContainer extends Component {
 
 	render() {
-		return (
-			<div className="NewsContainer">
-				{this.props.news.map(n =>
-					<News
-						newsItem={n}
-						key={n.link}
-					/>
-				)}
-			</div>
-		)
+		if (this.props.news.length > 0) {
+			return (
+				<div className="NewsContainer">
+					{this.props.news.map(n =>
+						<News
+							newsItem={n}
+							key={n.link}
+						/>
+					)}
+				</div>
+			)
+		} else {
+			return <p className="NewsContainer-no_news">Jelenleg ezzel a szűréssel nincsenek hírek</p>
+		}
 	}
 }
 
